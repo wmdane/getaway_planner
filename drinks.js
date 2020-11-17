f275c0bf163f921943ef11cf656d8b3ce2cf1295
Paul Carr <pwcarr15@gmail.com>
2020-11-17 15:00:16 -0500

    Drink.js now has skeleton to search based on city & state. It can filter based on the number of results user desires and has started to display results to webpage. It will do this once the eventlistener for a user 'click' is heard.

--- drinks.js
+++ drinks.js
@@ -1,16 +1,25 @@
 var APIKey = "&rapidapi-key=fd37675a80msh7185028ba8850b5p1ade34jsnda4a447c074a";
 
+$("#some-id").on("click", function() {
 
-
-    var citySearched = $("#user-input")
-    var queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/autocomplete?query=" + citySearched + APIKey;
+    var citySearched = $("#some-input");
+    var stateSearched = $("#some-input");
+    var queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=micro" + APIKey;
 
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        console.log(queryURL);
-        console.log(response);
-        
+        // console.log(queryURL);
+        // console.log(response);
+        var nc = response.filter(function(brewery) {
+            return brewery.state === "North Carolina" && brewery.city === "Charlotte"
+        })
+        console.log(nc);
+        var numberResults = $("#some-value").val();
+        for (var i = 0; i < numberResults; i++) {
+            var name = nc[i].name
+            $("#div").append(name);
+        }    
     })
-
+})
