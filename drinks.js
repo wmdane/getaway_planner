a93a780fe3586c39b6138b149deb63191cba92a1
Paul Carr <pwcarr15@gmail.com>
2020-11-18 11:38:23 -0500

    Results will now come back by information per brewery looked up and display information in a box.

--- drinks.js
+++ drinks.js
@@ -1,6 +1,6 @@
 var APIKey = "&rapidapi-key=fd37675a80msh7185028ba8850b5p1ade34jsnda4a447c074a";
 
-$("#some-id").on("click", function() {
+// $("#some-id").on("click", function() {
 
     var citySearched = $("#some-input");
     var stateSearched = $("#some-input");
@@ -12,14 +12,33 @@ $("#some-id").on("click", function() {
     }).then(function (response) {
         // console.log(queryURL);
         // console.log(response);
-        var nc = response.filter(function(brewery) {
+        var result = response.filter(function(brewery) {
             return brewery.state === "North Carolina" && brewery.city === "Charlotte"
         })
-        console.log(nc);
-        var numberResults = $("#some-value").val();
+        console.log(result);
+        var numberResults = 10;        //$("#some-value").val();
         for (var i = 0; i < numberResults; i++) {
-            var name = nc[i].name
-            $("#div").append(name);
-        }    
+            var cityBox = $("<div>");
+            cityBox.addClass("box");
+            var nameLine = $("<p>");
+            nameLine.text(name);
+            var addressLine = $("<p>");
+            addressLine.text(address);
+            var phoneLine = $("<p>");
+            phoneLine.text(phoneNum);
+            var websiteLine = $("<p>");
+            websiteLine.text(website);
+            var name = "Brewery: " + result[i].name;
+            var website = "Website: " + result[i].website_url;
+            var phoneNum = "Phone Number: " + result[i].phone;
+            var street = result[i].street;
+            var city = result[i].city;
+            var state = result[i].state;
+            var zip = result[i].postal_code;
+            var address = "Address: " + street + ", " + city + ", " + state + zip
+            cityBox.append(name, address, phoneNum, website);
+            $("#div").append(cityBox)
+        }   
+        
     })
-})
+// })
