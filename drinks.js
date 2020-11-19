d78182aa93267febfdfbe94f0d7df751f3d2fe37
Will Dane <enadlliw@gmail.com>
2020-11-19 14:09:02 -0500

    erase me

--- drinks.js
+++ drinks.js
@@ -1,93 +1,118 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var APIKey = "&rapidapi-key=fd37675a80msh7185028ba8850b5p1ade34jsnda4a447c074a";
 
-    var APIKey = "&rapidapi-key=fd37675a80msh7185028ba8850b5p1ade34jsnda4a447c074a";
+  $("#submitBtn").on("click", function () {
+    var citySearched = $("#cityInput").val();
+    var stateSearched = $("#stateInput").val();
+    var userInput = citySearched + ", " + stateSearched;
+    var queryURL =
+      "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=micro" + APIKey;
 
-    $("#submitBtn").on("click", function() {
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      // console.log(queryURL);
+      // console.log(response);
 
-        var citySearched = $("#cityInput").val();
-        var stateSearched = $("#stateInput").val();
-        var userInput = citySearched + ", " + stateSearched;
-        var queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=micro" + APIKey;
+      var result = response.filter(function (brewery) {
+        return brewery.state === stateSearched && brewery.city === citySearched;
+      });
 
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function (response) {
-            // console.log(queryURL);
-            // console.log(response);
+      //console.log(result);
+      if (result.length === 0) {
+        $("#displayDrinkResults").text(
+          "Sorry, Open Breweries DB did not return any micro breweries for this city. Search another city, OR"
+        );
+        function renderHistory() {
+          var array = [];
+          array.push(userInput);
+          for (var i = 0; i < array.length; i++) {
+            var b = $("<button>");
+            b.addClass("search-button");
+            localStorage.setItem(JSON.stringify("cities", array[i]));
+            var input = JSON.parse(localStorage.getItem("cities"));
+            b.text(input);
+            $("#displaySearchHistory").append(b);
+          }
+        }
+        renderHistory();
+        nonSearch();
+      } else {
+        //var numberResults = 10;
+        $("#displayDrinkResults").html("");
+        function renderHistory() {
+          var array = [];
+          array.push(userInput);
+          for (var i = 0; i < array.length; i++) {
+            var b = $("<button>");
+            b.addClass("search-button");
+            localStorage.setItem(JSON.stringify("cities", array[i]));
+            var input = JSON.parse(localStorage.getItem("cities"));
+            b.text(input);
+            $("#displaySearchHistory").append(b);
+          }
+        }
+        renderHistory();
+        for (var i = 0; i < result.length; i++) {
+          var cityBox = $("<div>");
+          cityBox.addClass("box");
 
-            var result = response.filter(function(brewery) {
-                return brewery.state === stateSearched && brewery.city === citySearched;
-            })
+          var nameLine = $("<h3>");
+          var name = "Brewery: " + result[i].name;
+          nameLine.text(name);
 
-            //console.log(result);
-            if (result.length === 0) {
-                $("#displayDrinkResults").text("Sorry, Open Breweries DB did not return any micro breweries for this city. Search another city, OR");
-                localStorage.setItem('userInput', userInput);
-                renderHistory();
-                nonSearch();
-            }
-            else {
-                //var numberResults = 10;
-                $("#displayDrinkResults").html("");
-                localStorage.setItem('userInput', userInput);
-                renderHistory();
-                for (var i = 0; i < result.length; i++) {
-                    var cityBox = $("<div>");
-                    cityBox.addClass("box");
+          var addressLine = $("<p>");
+          var street = result[i].street;
+          var city = result[i].city;
+          var state = result[i].state;
+          var zip = result[i].postal_code;
+          var address = "Address: " + street + ", " + city + ", " + state + " " + zip;
+          addressLine.text(address);
 
-                    var nameLine = $("<h3>");
-                    var name = "Brewery: " + result[i].name;
-                    nameLine.text(name);
+          var phoneLine = $("<p>");
+          var phoneNum = "Phone Number: " + result[i].phone;
+          phoneLine.text(phoneNum);
 
-                    var addressLine = $("<p>");
-                    var street = result[i].street;
-                    var city = result[i].city;
-                    var state = result[i].state;
-                    var zip = result[i].postal_code;
-                    var address = "Address: " + street + ", " + city + ", " + state + " " + zip;
-                    addressLine.text(address);
+          var websiteLine = $("<a>");
+          var website = result[i].website_url;
+          websiteLine.attr("href", website);
+          websiteLine.text("Brewery Website: " + website);
 
-                    var phoneLine = $("<p>");
-                    var phoneNum = "Phone Number: " + result[i].phone;
-                    phoneLine.text(phoneNum);
+          cityBox.append(nameLine, addressLine, phoneLine, websiteLine);
 
-                    var websiteLine = $("<a>");
-                    var website = result[i].website_url;
-                    websiteLine.attr("href", website);
-                    websiteLine.text("Brewery Website: " + website);
-                
-                    cityBox.append(nameLine, addressLine, phoneLine, websiteLine);
-                
-                    $("#displayDrinkResults").append(cityBox);
+          $("#displayDrinkResults").append(cityBox);
 
-                    
-                    console.log(userInput);
-                    
-                };                 
+          console.log(userInput);
+        }
+      }
 
-            };
-            
-            function renderHistory() {
-            
-                var b = $("<button>");
-                b.addClass("search-button");
-                var input = localStorage.getItem('userInput');
-                b.text(input);
-                $("#displaySearchHistory").append(b);
-            };
-
-        });
-
-        function nonSearch() {
-            var yelpURL = "https://www.yelp.com/search?find_desc=Breweries&find_loc=" + citySearched + "%2C+" + stateSearched + "&ns=1";
-            var messageBox = $("<div>");
-            messageBox.addClass("box");
-            var yelpLink = $("<a>");
-            yelpLink.attr("href", yelpURL);
-            yelpLink.text("Try Yelp instead");
-            messageBox.append(yelpLink);
-            $("#displayDrinkResults").append(messageBox);
-        };
+      function renderHistory() {
+        array.push(userInput);
+        for (var i = 0; i < array.length; i++) {
+          var b = $("<button>");
+          b.addClass("search-button");
+          var input = JSON.parse(localStorage.getItem("cities"));
+          b.text(input);
+          $("#displaySearchHistory").append(b);
+        }
+      }
     });
+
+    function nonSearch() {
+      var yelpURL =
+        "https://www.yelp.com/search?find_desc=Breweries&find_loc=" +
+        citySearched +
+        "%2C+" +
+        stateSearched +
+        "&ns=1";
+      var messageBox = $("<div>");
+      messageBox.addClass("box");
+      var yelpLink = $("<a>");
+      yelpLink.attr("href", yelpURL);
+      yelpLink.text("Try Yelp instead");
+      messageBox.append(yelpLink);
+      $("#displayDrinkResults").append(messageBox);
+    }
+  });
 });
